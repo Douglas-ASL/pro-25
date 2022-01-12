fbf28efc1d737227e01b1ab555dacfbf8a1b95f8
Douglas-ASL <94068133+Douglas-ASL@users.noreply.github.com>
2022-01-12 08:05:43 -0300

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,33 @@
+class Paper{ 
+
+    constructor(){
+  var options={
+	isStatic:false,			
+	restitution:0.3,	
+    friction:0,
+    density:1.2
+}
+
+this.x=200;
+this.y=300;
+this.r=30;
+
+this.image=loadImage("paper.png");
+
+
+this.body = Bodies.circle(this.x, this.y, (this.r-20)/2, options);
+World.add(world, this.body);
+
+
+}
+display(){
+var paperpos=this.body.position;		
+push();
+translate(paperpos.x, paperpos.y);
+ellipseMode(CENTER)
+ellipse(0,0,this.r,this.r);
+pop();
+}
+
+
+}
